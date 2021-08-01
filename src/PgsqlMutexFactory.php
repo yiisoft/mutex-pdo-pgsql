60da56960f3a8dd22a4ee743aa319069eb47b9d8
Alexander Makarov <sam@rmcreative.ru>
2021-08-01 20:03:02 +0300

    Use MutexFactory

--- src/PgsqlMutexFactory.php
+++ src/PgsqlMutexFactory.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Yiisoft\Mutex\File;
 
 use PDO;
-use Yiisoft\Mutex\MutexFactoryInterface;
+use Yiisoft\Mutex\MutexFactory;
 use Yiisoft\Mutex\MutexInterface;
 use Yiisoft\Mutex\PgsqlMutex;
 
 /**
  * Allows creating {@see PgsqlMutex} mutex objects.
  */
-final class PgsqlMutexFactory implements MutexFactoryInterface
+final class PgsqlMutexFactory extends MutexFactory
 {
     private PDO $connection;
     private bool $autoRelease;
